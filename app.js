87a532273c26cb2bdfe7acbf5465ea0c0cde4109
comradesurendra <surendrakumarjuet@gmail.com>
2020-06-15 22:06:22 +0530

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 const express=require('express');
+const novelcovid=require('novelcovid');
+const expresshandlebar=require('express-handlebars');
+
+
+const app=express();
+app.engine('handlebars',expresshandlebar({defaultLayout:'main'}));
+app.set('view engine','handlebars')
+
+novelcovid.countries().then((response) => {
+    console.log(response);
+    });
+    
+
+app.get("/", function (req, res) {
+    novelcovid.countries().then((response) => {
+    res.render("home", { info: response });
+    });
+    });
+
+app.listen('3535',()=>{
+    console.log('Server started at port 3535');
+    
+})
